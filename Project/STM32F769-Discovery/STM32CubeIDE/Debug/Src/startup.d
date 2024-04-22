d2846ffd0099071e68be6c349ddbb261cd128b93
FireFly <148460208+FireFly08151@users.noreply.github.com>
2024-04-22 18:01:21 +0200

    Delete Project/STM32F769-Discovery/STM32CubeIDE/Debug directory

--- Project/STM32F769-Discovery/STM32CubeIDE/Debug/Src/startup.d
+++
@@ -1,4 +0,0 @@
-Src/startup.o: \
- D:/GitHub/TikTakToe/Project/STM32F769-Discovery/STM32CubeIDE/startup.s \
- C:/Users/veija/OneDrive\ -\ Hochschule\ Bonn-Rhein-Sieg/Studium/C++/EmbSysLib/Src/Hardware/MCU/STM32F7xx/Sys/GCC/startup_stm32f769nihx.s
-C:/Users/veija/OneDrive\ -\ Hochschule\ Bonn-Rhein-Sieg/Studium/C++/EmbSysLib/Src/Hardware/MCU/STM32F7xx/Sys/GCC/startup_stm32f769nihx.s:
